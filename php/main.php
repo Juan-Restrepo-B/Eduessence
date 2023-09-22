6a11068db388e0df789929f589a63fa92a76cd59
Juan-Restrepo-B <juanprestrepobq@gmail.com>
2023-09-22 11:20:55 -0500

    correccion de nombres

--- php/main.php
+++ php/main.php
@@ -96,7 +96,7 @@ function showButtons($buttonNumber)
                 <?php //endif; ?>
                 <?php //if (showButtons(2)): ?>
                 <div class="icon-container">
-                    <a href="../documet/index.php" class="menu-icon" target="contentMain">
+                    <a href="../document/index.php" class="menu-icon" target="contentMain">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                             style="fill: rgba(0, 0, 0, 1);">
                             <path
