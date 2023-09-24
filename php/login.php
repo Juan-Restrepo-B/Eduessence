f1869448cb25ff38bdd5eef720fa9a555ad4b96b
jkiol227 <diegobastidasa@gmail.com>
2023-09-23 22:45:43 -0500

    svg correction login

--- php/login.php
+++ php/login.php
@@ -146,7 +146,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         <?php if (isset($error_message)): ?>
                             <span style="color: <?= $color; ?>">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24"
-                                    style="fill: rgba(255, 0, 0, 1);transform: ;msFilter:;">
+                                    style="fill: rgba(255, 0, 0, 1);">
                                     <path
                                         d="M11.953 2C6.465 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.493 2 11.953 2zM12 20c-4.411 0-8-3.589-8-8s3.567-8 7.953-8C16.391 4 20 7.589 20 12s-3.589 8-8 8z">
                                     </path>
