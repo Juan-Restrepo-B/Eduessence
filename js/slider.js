e774acc4bd5fcb8c447f1771afa6468f2b4d8cfc
Juan-Restrepo-B <juanprestrepobq@gmail.com>
2023-09-24 19:33:13 -0500

    Front observaciones y cambios

--- js/slider.js
+++ js/slider.js
@@ -6,8 +6,7 @@ setInterval(function(){
   slide1.classList.toggle('invisible');
   slide2.classList.toggle('invisible');
   slide2.classList.toggle('visible');
-},20000);
-
+}, 30000); // Cambio cada 30 segundos (30000 milisegundos)
 
 const titleslide1 = document.querySelector('#title-slide1');
 const titleslide2 = document.querySelector('#title-slide2');
@@ -17,4 +16,4 @@ setInterval(function(){
   titleslide1.classList.toggle('invisible');
   titleslide2.classList.toggle('invisible');
   titleslide2.classList.toggle('visible');
-},20000);
+}, 30000); // Cambio cada 30 segundos (30000 milisegundos)
