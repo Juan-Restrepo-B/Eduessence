f08690d02deb2e65d25b5208b50a62cdddc79578
Juan-Restrepo-B <juanprestrepob@gmail.com>
2023-10-01 15:04:17 -0500

    Funcionalidad de los diplomas completa

--- diploma/index.php
+++ diploma/index.php
@@ -5,6 +5,7 @@ include('conexion.php');
 
 //$usuario = $_SESSION['useremail'];
 $ip_cliente = $_SERVER['REMOTE_ADDR'];
+$useremail = $_SESSION['useremail'];
 
 $sql = "SELECT CARRERA_USUARIO_NOMBRE, 
         CARRERA_IDCURSO, CURSO_NOMBRE, 
@@ -16,7 +17,7 @@ $sql = "SELECT CARRERA_USUARIO_NOMBRE,
         INNER JOIN TR_CURSOS tc 
         ON uc.CARRERA_IDCURSO = tc.IDCURSOS 
         WHERE CARRERA_CERTESTADO = 'SI'
-        AND CARRERA_USUARIO_NOMBRE = 'juanprestrepob@gmail.com'"; //" . $usuario . "
+        AND CARRERA_USUARIO_NOMBRE = '" . $useremail . "'";
 $result = $conn->query($sql);
 
 ?>
@@ -47,8 +48,8 @@ $result = $conn->query($sql);
                                 <?php echo strtoupper($row["CURSO_NOMBRE"]); ?>
                             </h2>
                             <div class="btn">
-                                <a href="#" target="_blank">Ver</a>
-                                <a href="#">Descargar</a>
+                                <a href="certificados/Diplomas.php?idUser=<?php echo strtoupper($row["CARRERA_USUARIO_NOMBRE"]); ?>&tipA=<?php echo strtoupper($row["CARRERA_IDTIPARTICIPANTE"]); ?>&idCurso=<?php echo strtoupper($row["CARRERA_IDCURSO"]); ?>" target="_blank">Ver</a>
+                                <a href="certificados/Diplomad.php?idUser=<?php echo strtoupper($row["CARRERA_USUARIO_NOMBRE"]); ?>&tipA=<?php echo strtoupper($row["CARRERA_IDTIPARTICIPANTE"]); ?>&idCurso=<?php echo strtoupper($row["CARRERA_IDCURSO"]); ?>">Descargar</a>
                             </div>
                         </div>
                         <?php
