3824b1346acd5bbd2fa20fd4485b6227a71beb13
Juan-Restrepo-B <juanprestrepob@gmail.com>
2023-10-01 15:34:36 -0500

    Diplomas

--- diploma/index.php
+++ diploma/index.php
@@ -48,8 +48,8 @@ $result = $conn->query($sql);
                                 <?php echo strtoupper($row["CURSO_NOMBRE"]); ?>
                             </h2>
                             <div class="btn">
-                                <a href="certificados/Diplomas.php?idUser=<?php echo strtoupper($row["CARRERA_USUARIO_NOMBRE"]); ?>&tipA=<?php echo strtoupper($row["CARRERA_IDTIPARTICIPANTE"]); ?>&idCurso=<?php echo strtoupper($row["CARRERA_IDCURSO"]); ?>" target="_blank">Ver</a>
-                                <a href="certificados/Diplomad.php?idUser=<?php echo strtoupper($row["CARRERA_USUARIO_NOMBRE"]); ?>&tipA=<?php echo strtoupper($row["CARRERA_IDTIPARTICIPANTE"]); ?>&idCurso=<?php echo strtoupper($row["CARRERA_IDCURSO"]); ?>">Descargar</a>
+                                <a href="certificados/Diplomas.php?idUser=<?php echo $row["CARRERA_USUARIO_NOMBRE"]; ?>&tipA=<?php echo $row["CARRERA_IDTIPARTICIPANTE"]; ?>&idCurso=<?php echo $row["CARRERA_IDCURSO"]; ?>" target="_blank">Ver</a>
+                                <a href="certificados/Diplomas.php?idUser=<?php echo $row["CARRERA_USUARIO_NOMBRE"]; ?>&tipA=<?php echo $row["CARRERA_IDTIPARTICIPANTE"]; ?>&idCurso=<?php echo $row["CARRERA_IDCURSO"]; ?>">Descargar</a>
                             </div>
                         </div>
                         <?php
