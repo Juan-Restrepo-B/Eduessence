e5610c5d78bdc7a3ed17b2fbb62cffe1a9c2854a
Juan-Restrepo-B <juanprestrepobq@gmail.com>
2023-10-27 20:49:32 -0500

    Cambio de palabra mal escrita en la seccion de servicios y la integracion de los del summit 21

--- js/redirect.js
+++ js/redirect.js
@@ -3,8 +3,12 @@ function redireccionarALogin() {
     window.location.href = "php/login.php";
 }
 
+function redireccionarALogin2() {
+    window.location.href = "login.php";
+}
+
 function redireccionarASummit21() {
-    window.location.href = "php/login.php";
+    window.location.href = "php/summit21.php";
 }
 
 function redireccionarASummit23() {
