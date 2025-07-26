05bf70c58ca2f0b8f85094241e967081217a4138
Juan-Restrepo-B <juanprestrepob@gmail.com>
2025-07-26 14:19:33 -0500

    Creaciones validaciones congreso

--- view/js/index/congress_attendance.js
+++ view/js/index/congress_attendance.js
@@ -79,9 +79,9 @@ window.onload = function () {
         }
 
         // Función para establecer una cookie
-        function setCookie(name, value, days) {
-            const date = new Date();
-            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-            const expires = "expires=" + date.toUTCString();
-            document.cookie = name + "=" + value + ";" + expires + ";path=/";
-        }
+        // function setCookie(name, value, days) {
+        //     const date = new Date();
+        //     date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        //     const expires = "expires=" + date.toUTCString();
+        //     document.cookie = name + "=" + value + ";" + expires + ";path=/";
+        // }
